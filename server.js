62a29952bcf3b049ea32c8142bd512059e428861
Osita Ezeigbo <osiclin@yahoo.com>
2023-09-21 00:00:17 +0100

    implement build

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const cors = require('cors')
 const fs = require('fs')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 5000
 
+const buildPath = path.join(__dirname, 'build')
+
+app.use(express.static(buildPath))
 app.use(cors())
+
 app.get('/api/v1/books', (req, res) => {
     try {
         const { tableName } = req.query
@@ -15,6 +20,10 @@ app.get('/api/v1/books', (req, res) => {
     } catch (error) {
         console.log(error)
     }
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildPath, 'index.html'))
 })
 
 app.listen(port, () => {
